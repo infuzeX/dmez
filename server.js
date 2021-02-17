d72808424fb5ccebbf5c7eae51959c0e2f415bdb
codernuub <sardarbhai6@gmail.com>
2021-02-18 02:26:28 +0530

    release1

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 const PORT = process.env.POST || 3001;
 
-app.listen(PORT, () => console.log("up and running"));
+app.listen(PORT, () => console.log("up and running on heroku"));
