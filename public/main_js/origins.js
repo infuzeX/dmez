fd1fbc29e501e99ce9b79b1573a5d85aa1a5e238
codernuub <sardarbhai6@gmail.com>
2021-02-18 02:47:02 +0530

    added origin switcher2

--- public/main_js/origins.js
+++ public/main_js/origins.js
@@ -1,8 +1,8 @@
 const origins = {
-     api:["http://127.0.0.1:3000", "https://dmezapi.herokuapp.com"],
-     img:["http://127.0.0.1:4000", "https://dmez.herokuapp.com"],
-     getApi:function(data) {
-        const prod = window.location.protocol.startsWith('https');
-        return data[Number(prod)];
-     }
+   api: ["http://127.0.0.1:3000", "https://dmezapi.herokuapp.com"],
+   img: ["http://127.0.0.1:4000", "https://dmez.herokuapp.com"],
+   getApi: function (data) {
+      const prod = window.location.protocol.startsWith('https');
+      return this[data][prod];
+   }
 }
